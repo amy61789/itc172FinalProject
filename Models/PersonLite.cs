20e6bb85606f208fcb7a1baf00764507ebf9e9b7
amy61789 <amy61789@outlook.com>
2017-03-15 10:52:29 -0700

    Models yo

---
+++ Models/PersonLite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAssignment.Models
+{
+    public class PersonLite
+    {
+        public int PersonKey { set; get; }
+        public string LastName { set; get; }
+        public string FirstName { set; get; }
+        public string Email { set; get; }
+        public string Apartment { set; get; }
+        public string Street { set; get; }
+        public string City { set; get; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string HomePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Password { get; set; }
+    }
+}
